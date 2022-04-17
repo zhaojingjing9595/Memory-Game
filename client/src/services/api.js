df9e1d15cf19811c0152a8cfe7ef9a3d91dd9cbc
zhaojingjing95 <77484772+zhaojingjing95@users.noreply.github.com>
2022-04-17 21:18:53 +0300

    add login and signup api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,1 +1,19 @@
 import axios from "axios";
+const api = axios.create({ baseURL: "http://localhost:8080" });
+
+async function login(email, password) {
+    const response = await api.post("/auth/login", { email, password });
+    return response.data;
+}
+
+async function signUp(nickName, email, pwd, confirmPwd) { 
+    const response = await api.post("/auth/signup", {
+      nickName,
+      email,
+      pwd,
+      confirmPwd,
+    });
+    return response.data
+}
+
+export default { login, signUp };
