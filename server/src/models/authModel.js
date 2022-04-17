46c11b97a319279a43a2316884c18c27096be454
zhaojingjing95 <77484772+zhaojingjing95@users.noreply.github.com>
2022-04-17 22:10:14 +0300

    authModel add login, signup

--- server/src/models/authModel.js
+++ server/src/models/authModel.js
@@ -1,10 +1,23 @@
+import { appDB } from "../index.js";
+
 async function login(email, password) {
-    
-    
+  const result = await appDB.from("users").where({ email, password });
+  console.log(result);
+  return result;
 }
 
 async function signup(nickName, email, pwd, confirmPwd) {
-
+  const newUser = {
+    nickName: nickName,
+    email: email,
+    password: pwd,
+    rePassword: confirmPwd,
+  };
+  const userId = await appDB.from("users").insert(newUser);
+  console.log(userId[0]);
+  const user = await appDB.from("users").where({ id: userId[0] });
+  console.log(user);
+  return user;
 }
 
-export default {login, signup}
+export default { login, signup };
