b639297e894da8aab632b8e5eb98befead5f2db6
alanlipschutz <alanelipschutz@gmail.com>
2022-04-17 16:03:10 +0300

    shuffling cards

--- client/src/Pages/Game/Game.jsx
+++ client/src/Pages/Game/Game.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Button } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 
 const cardImages = [
   { src: "/img/helmet-1.png" },
@@ -7,16 +11,27 @@ const cardImages = [
   { src: "/img/scroll-1.png" },
   { src: "/img/shield-1.png" },
   { src: "/img/sword-1.png" },
-  { src: "/img/cover.png" },
 ];
 
 function Game() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
   function shuffleCards() {
-    const shuffledCards = [...cardImages, ...cardImages].sort(
-      () => Math.random() - 0.5
-    );
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: uuidv4() }));
+    setCards(shuffledCards);
+    setTurns(0);
   }
-  return <div></div>;
+  return (
+    <div>
+      <h1 className="display-4 my-3">Magic Match</h1>
+      <Button variant="warning" onClick={shuffleCards}>
+        Start the Game
+      </Button>{" "}
+    </div>
+  );
 }
 
 export default Game;
