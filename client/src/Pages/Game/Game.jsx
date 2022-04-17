60b61a88dd15eae30c7904caa60ba21cdf9774ff
alanlipschutz <alanelipschutz@gmail.com>
2022-04-17 15:43:52 +0300

    added cardImages

--- client/src/Pages/Game/Game.jsx
+++ client/src/Pages/Game/Game.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+const cardImages = [
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
+  { src: "/img/cover.png" },
+];
+
 function Game(props) {
   return <div></div>;
 }
